21bbf0fb29c5fa6dd30ac775d0a8da03af00166d
joao pedro alves dos santos <joaopedroalvessantos@gmail.com>
2026-04-13 17:19:08 -0300

    feat(p1): obtem condicoes atuais a partir de coord

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const axios = require('axios')
 
-const baseURL = 'https://geocoding-api.open-meteo.com/v1/search'
-// https://geocoding-api.open-meteo.com/v1/search?name=Berlin&count=10&language=en&format=json
+const geoCodingBaseURL = 'https://geocoding-api.open-meteo.com/v1/search'
+
+const forecastBaseURL = 'https://api.open-meteo.com/v1/forecast'
+
 const name = 'Berlin'
 
 const count = 10  
@@ -10,9 +12,9 @@ const lang = 'pt-br'
 
 const format = 'json'  
 
-const url = `${baseURL}?name=${name}&count=${count}&language=${lang}&format=${format}`
+const urlGeoCoding = `${geoCodingBaseURL}?name=${name}&count=${count}&language=${lang}&format=${format}`
 
-axios.get(url)
+axios.get(urlGeoCoding)
 .then(res => {
     return res.data
 })
@@ -24,4 +26,21 @@ axios.get(url)
     console.log(results[0].longitude)
     const lat = results[0].latitude
     const long = results[0].longitude
+    const urlForecast = `${forecastBaseURL}?latitude=${lat}&longitude=${long}&current=temperature_2m,apparent_temperature,wind_speed_10m`
+    const climaTempo = async() => {
+        try {
+            const res = await axios.get(urlForecast)
+            //console.log(res)
+            console.log(res.data.current.temperature_2m)
+            console.log(res.data.current.apparent_temperature)  
+            console.log(res.data.current.wind_speed_10m)
+            const tempAtual = res.data.current.temperature_2m
+            const sensacaoTermica = res.data.current.apparent_temperature
+            const velocidadeVento = res.data.current.wind_speed_10m
+        } catch (error) {
+            console.log(`Erro: ${error}`)
+        }
+    }
+    climaTempo()
+
 })
